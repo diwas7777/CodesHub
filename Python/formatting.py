55103d2c2466d2f62fbc4ab159b0e6f521eef203
sidson1 <72189602+sidson1@users.noreply.github.com>
2022-10-08 18:37:39 +0530

    Create formatting.py
    i created formatting.py

---
+++ Python/formatting.py
@@ -0,0 +1,15 @@
+# Python program showing how to use
+# string modulo operator(%) to print
+# fancier output
+
+# print integer and float value
+print("Geeks : %2d, Portal : %5.2f" % (1, 05.333))
+
+# print integer value
+print("Total students : %3d, Boys : %2d" % (240, 120))
+
+# print octal value
+print("%7.3o" % (25))
+
+# print exponential value
+print("%10.3E" % (356.08977))
