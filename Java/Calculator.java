658326255577316a71b65059c2fc84fd811fd77b
Dipesh0-0 <85897484+Dipesh0-0@users.noreply.github.com>
2022-10-08 16:28:06 +0545

    fixed bug

--- Java/Calculator.java
+++ Java/Calculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Calc {
+public class Calculator {
 public static void main(String[] args){
     System.out.println("Enter your problem");
     Scanner input = new Scanner(System.in);
