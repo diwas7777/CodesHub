2e8559fcdba68752d9a01c08170b79fb0fe1fade
Jeffrey Norris <jeffreydn1161@gmail.com>
2022-10-10 15:15:33 -0400

    Update the generation function.

--- Python/password_generator.py
+++ Python/password_generator.py
@@ -1,75 +1,99 @@
 import random
 import string
 
-avail_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-avail_symbols = ['!', '(', ')', '?', '[', ']', '_', '`', '~', ';', ':', '@', '#', '$', '%', '^', '&', '*', '+', '=']
+class PasswordTools:
+    
+    def __init__(self) -> None:
+        self.my_password = ""
+        self.pass_len = len(my_password)
 
-# Take user input for type of password
-def set_options():
-    global pass_len
-    global upper_bool
-    global lower_bool
-    global number_bool
-    global symbol_bool
-    while(True):
-        pass_len = input("How long do you want your password? (6-35): ").strip()
-        try:
-            pass_len = int(pass_len)
-            if(pass_len < 6 or pass_len > 35):
-                print("Please choose a number between 6-35")
+    def __get_upper(self):
+        return string.ascii_uppercase[random.randint(0, 25)]
+
+    def __get_lower(self):
+        return string.ascii_lowercase[random.randint(0, 25)]
+
+    def __get_number(self):
+        avail_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        return str(avail_numbers[random.randint(0, len(avail_numbers)-1)])
+
+    def __get_symbol(self):
+        avail_symbols = ['!', '(', ')', '?', '[', ']', '_', '`', '~', ';', ':', '@', '#', '$', '%', '^', '&', '*', '+', '=']
+        return str(avail_symbols[random.randint(0, len(avail_symbols)-1)])
+
+    def start(self):
+        #Ask the user what they would like to do.
+        #Currently there is only password generation.
+        self.generate_password()
+        print(self.get_password())
+
+    def set_options(self):
+        # Ask for desired password length
+        while(True):
+            self.pass_len = input("How long do you want your password? (6-35): ").strip()
+            try:
+                self.pass_len = int(self.pass_len)
+                if(self.pass_len < 6 or self.pass_len > 35):
+                    print("Please choose a number between 6-35")
+                else:
+                    break
+            except ValueError:
+                print("That is not a number.")
+
+        # Ask for upper and lower case (must choose atleast 1 option)
+        upper_bool = input("Do you want uppercase letters? (y/n): ").lower().strip() == 'y'
+        lower_bool = input("Do you want lowercase letters? (y/n): ").lower().strip() == 'y'
+        while (not upper_bool and not lower_bool):
+            print ("You must select atleast upper or lower case")
+            ans = (input("(upper, lower, both): ").lower().strip())
+            if(ans == 'upper'):
+                upper_bool = True
+            elif(ans == 'lower'):
+                lower_bool = True
+            elif(ans == 'both'):
+                upper_bool = True
+                lower_bool = True
             else:
-                break
-        except ValueError:
-            print("That is not a number.")
-            
+                print ("I don't understand.")
 
-    upper_bool = input("Do you want uppercase letters? (y/n): ").lower().strip() == 'y'
-    lower_bool = input("Do you want lowercase letters? (y/n): ").lower().strip() == 'y'
-    while (not upper_bool and not lower_bool):
-        print ("You must select atleast upper or lower case")
-        ans = (input("(upper, lower, both): ").lower().strip())
-        if(ans == 'upper'):
-            upper_bool = True
-        elif(ans == 'lower'):
-            lower_bool = True
-        elif(ans == 'both'):
-            upper_bool = True
-            lower_bool = True
-        else:
-            print ("I don't understand.")
-    number_bool = input("Do you want numbers? (y/n): ").lower().strip() == 'y'
-    symbol_bool = input("Do you want symbols? (y/n): ").lower().strip() == 'y'
+        # Ask for numbers and symbols
+        number_bool = input("Do you want numbers? (y/n): ").lower().strip() == 'y'
+        symbol_bool = input("Do you want symbols? (y/n): ").lower().strip() == 'y'
 
-def chosen_options():
-    set_options()
-    print("Password will be " + str(pass_len) + " characters long, and contain:")
-    print("\tUppercase Letters: " + str(upper_bool))
-    print("\tLowercase Letters: " + str(lower_bool))
-    print("\tNumbers: " + str(number_bool))
-    print("\tSymbols: " + str(symbol_bool))
+        # Create array of chosen options
+        pass_options = []
+        if(upper_bool):
+            pass_options.append(self.__get_upper)
+        if(lower_bool):
+            pass_options.append(self.__get_lower)
+        if(number_bool):
+            pass_options.append(self.__get_number)
+        if(symbol_bool):
+            pass_options.append(self.__get_symbol)
 
-# Generate password of just letters
-def generate_simple_password():
-    global my_password
-    my_password = ""
-    counter = 0
-    while(counter < pass_len):
-        if(upper_bool and lower_bool):
-            flip = randint(0, 1)
-            if(flip):
-                my_password += string.ascii_uppercase[random.randint(0, 25)]
-            else:
-                my_password += string.ascii_lowercase[random.randint(0, 25)]
-        elif(upper_bool):
-            my_password += string.ascii_uppercase[random.randint(0, 25)]
-        else:
-            my_password += string.ascii_lowercase[random.randint(0, 25)]
-        counter += 1
+        return pass_options
 
-# TODO generate password based on all inputs
+    # Generate a password with the selected options
+    def generate_password(self):
+        selected_options = self.set_options()
+        # Set first character to a letter
+        first_char = []
+        if(self.__get_upper in selected_options):
+            first_char.append(self.__get_upper)
+        if(self.__get_lower in selected_options):
+            first_char.append(self.__get_lower)
+        self.my_password = first_char[random.randint(0, len(first_char)-1)]()
+
+        # Loop through remaining length
+        counter = 1
+        while(counter < pass_len):
+            self.my_password += selected_options[random.randint(0, len(selected_options)-1)]()
+            counter += 1
+
+    def get_password(self):
+        return self.my_password
+
 # TODO Calculate password strength
 # TODO mutate a password into 1337spk
 
-set_options()
-generate_simple_password()
-print(my_password)
+PasswordTools().start()
