81456dbfc2e8e258e91b52b168d792e19e521b6f
harithadas010 <116432828+harithadas010@users.noreply.github.com>
2022-10-22 15:59:11 +0530

    Create area.py

---
+++ Python/area.py
@@ -0,0 +1,8 @@
+# Three sides of the triangle is a, b and c:
+a = float(input('Enter first side: '))
+b = float(input('Enter second side: '))
+c = float(input('Enter third side: '))
+# calculate the semi-perimeter.
+s = (a + b + c) / 2.
+# calculate the area.
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5.
