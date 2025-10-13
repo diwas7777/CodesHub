b24326070f8b1ca1442c2d4435cefd02b103d357
ti-v <thetivishasinha@gmail.com>
2025-10-12 22:30:11 -0700

    Add files via upload

---
+++ Python/basic-calc-tiv.py
@@ -0,0 +1,39 @@
+# LANGUAGE: Python 
+# ENV: Python 3.12
+# AUTHOR: Tivisha Sinha
+# GITHUB: https://github.com/ti-v/hacktoberfest
+
+def add(n1, n2):
+    return n1 + n2
+
+def sub(n1, n2):
+    return n1 - n2
+
+def mul(n1, n2):
+    return n1 * n2
+
+def div(n1, n2):
+    return n1 / n2
+
+print("Please select operation -\n"
+      "1. Add\n"
+      "2. Subtract\n"
+      "3. Multiply\n"
+      "4. Divide\n")
+
+sel = int(input("Select operation (1-4): "))
+
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+
+if sel == 1:
+    print(n1, "+", n2, "=", add(n1, n2))
+elif sel == 2:
+    print(n1, "-", n2, "=", sub(n1, n2))
+elif sel == 3:
+    print(n1, "*", n2, "=", mul(n1, n2))
+elif sel == 4:
+    print(n1, "/", n2, "=", div(n1, n2))
+else:
+    print("Invalid input. Please type in a number 1-4.")
+    
