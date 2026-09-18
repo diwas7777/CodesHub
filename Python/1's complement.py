296a9d4162bed63aec9685c6fa12f94ba4365307
AndreasAlexanderF <andreasforootan@fedora>
2026-09-18 11:18:22 -0400

    Fix ValueError in "Python/1's complement.py"

--- Python/1's complement.py
+++ Python/1's complement.py
@@ -1,10 +1,10 @@
-#intial part
-mainValue = (input("Enter main value ->")) 
+#User input
+mainValue = (input("Enter main value ->"))
 secondValue = (input("Enter second value ->"))
 
 print("making equal number of digits")
-while (len(mainValue) != len(secondValue)): #makes equal number of digits
-    if (len(mainValue) < len(secondValue)): #adds 0 to main value
+while len(mainValue) != len(secondValue): #makes equal number of digits
+    if len(mainValue) < len(secondValue): #adds 0 to main value
         mainValue = "0" + mainValue
 
     else:                                   #adds 0 to the second value
@@ -13,13 +13,14 @@ while (len(mainValue) != len(secondValue)): #makes equal number of digits
 print ("main value -> " + mainValue)
 print ("second value -> " + secondValue + "\n")
 
-#end of initial part
+
+#Finding 1's complement
 
 print("finding 1's complement of second value")
 firstList = list(secondValue)
 i = 0
 newSecondValue = ""
-while (i<len(firstList)):
+while i<len(firstList):
     convertedValue = 1 - int(firstList[i])
     if newSecondValue == "":
         newSecondValue = str(convertedValue)
@@ -28,45 +29,52 @@ while (i<len(firstList)):
     i+=1
 print ("-> " + newSecondValue + "\n")
 
-print("Adding main value to the complement value")
-sum = bin(int(newSecondValue, 2) + int(mainValue, 2))
 
-additionResult = sum[2:]
-print (newSecondValue + "+" + mainValue +"-> " + additionResult )
-i = 0
-secondList = list(additionResult)
-while len(mainValue) > len(additionResult):
-    additionResult = "0" + additionResult
+# Converting values to binary and finding the sum
+try:
+    sumBinary = bin(int(newSecondValue, 2) + int(mainValue, 2))
 
-if len(mainValue) != len(additionResult):
-    print ("overflow occured so removing overflow digit and adding it to remaining value")
-    remainingValue = ""
-    while (i<len(secondList)):
-        if i == 0:
-            overflowDigit = secondList[i]
-        elif remainingValue == "":
-            remainingValue = secondList[i]
-        else:
-            remainingValue = remainingValue + secondList[i]
-        i+=1
-
-    sum =  bin(int(remainingValue, 2) + int(overflowDigit, 2))
-    result = sum[2:]
-    print(mainValue + " - " + secondValue + " = " + result) 
-
+except ValueError:
+    print("Error: Your main value is not a binary number, please write a new value")
 
 else:
-    print("finding 1's complement of second value") 
-    thirdList = list(additionResult)
+    print("Adding main value to the complement value")
+
+    additionResult = sumBinary[2:]
+    print (newSecondValue + "+" + mainValue +"-> " + additionResult )
     i = 0
-    result = ""
-    while (i<len(thirdList)):
-        convertedValue = 1 - int(thirdList[i])
-        if result == "":
-            result = str(convertedValue)
-        else:
-            result = result + str(convertedValue)
-        i+=1
-    print(mainValue + " - " + secondValue + " = -" + result)
+    secondList = list(additionResult)
+    while len(mainValue) > len(additionResult):
+        additionResult = "0" + additionResult
+
+    if len(mainValue) != len(additionResult):
+        print ("overflow occured so removing overflow digit and adding it to remaining value")
+        remainingValue = ""
+        while i<len(secondList):
+            if i == 0:
+                overflowDigit = secondList[i]
+            elif remainingValue == "":
+                remainingValue = secondList[i]
+            else:
+                remainingValue = remainingValue + secondList[i]
+            i+=1
+
+        sumBinary =  bin(int(remainingValue, 2) + int(overflowDigit, 2))
+        result = sumBinary[2:]
+        print(mainValue + " - " + secondValue + " = " + result)
+
+    else:
+        print("finding 1's complement of second value")
+        thirdList = list(additionResult)
+        i = 0
+        result = ""
+        while (i<len(thirdList)):
+            convertedValue = 1 - int(thirdList[i])
+            if result == "":
+                result = str(convertedValue)
+            else:
+                result = result + str(convertedValue)
+            i+=1
+        print(mainValue + " - " + secondValue + " = -" + result)
 
 
